919be39dc600fefcd37656795e44b8f9ef570423
unknown <30065453@tafe.wa.edu.au>
2023-05-24 15:09:25 +0800

    import old Sprint1 and add buttons

--- AstrologicalCalculation/Form1.Designer.cs
+++ AstrologicalCalculation/Form1.Designer.cs
@@ -28,13 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ListBoxOutput = new System.Windows.Forms.ListBox();
+            this.ButtonSearch = new System.Windows.Forms.Button();
+            this.ButtonEdit = new System.Windows.Forms.Button();
+            this.ButtonSort = new System.Windows.Forms.Button();
+            this.TextBoxInput = new System.Windows.Forms.TextBox();
+            this.ButtonMidExtreme = new System.Windows.Forms.Button();
+            this.ButtonMode = new System.Windows.Forms.Button();
+            this.ButtonAverage = new System.Windows.Forms.Button();
+            this.ButtonRange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListBoxOutput
+            // 
+            this.ListBoxOutput.FormattingEnabled = true;
+            this.ListBoxOutput.Location = new System.Drawing.Point(466, 41);
+            this.ListBoxOutput.Name = "ListBoxOutput";
+            this.ListBoxOutput.Size = new System.Drawing.Size(296, 355);
+            this.ListBoxOutput.TabIndex = 0;
+            this.ListBoxOutput.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ListBoxOutput_MouseClick);
+            // 
+            // ButtonSearch
+            // 
+            this.ButtonSearch.Location = new System.Drawing.Point(156, 162);
+            this.ButtonSearch.Name = "ButtonSearch";
+            this.ButtonSearch.Size = new System.Drawing.Size(198, 44);
+            this.ButtonSearch.TabIndex = 1;
+            this.ButtonSearch.Text = "Search";
+            this.ButtonSearch.UseVisualStyleBackColor = true;
+            this.ButtonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
+            // 
+            // ButtonEdit
+            // 
+            this.ButtonEdit.Location = new System.Drawing.Point(156, 223);
+            this.ButtonEdit.Name = "ButtonEdit";
+            this.ButtonEdit.Size = new System.Drawing.Size(198, 44);
+            this.ButtonEdit.TabIndex = 2;
+            this.ButtonEdit.Text = "Edit";
+            this.ButtonEdit.UseVisualStyleBackColor = true;
+            this.ButtonEdit.Click += new System.EventHandler(this.ButtonEdit_Click);
+            // 
+            // ButtonSort
+            // 
+            this.ButtonSort.Location = new System.Drawing.Point(156, 288);
+            this.ButtonSort.Name = "ButtonSort";
+            this.ButtonSort.Size = new System.Drawing.Size(198, 44);
+            this.ButtonSort.TabIndex = 3;
+            this.ButtonSort.Text = "Sort";
+            this.ButtonSort.UseVisualStyleBackColor = true;
+            this.ButtonSort.Click += new System.EventHandler(this.ButtonSort_Click);
+            // 
+            // TextBoxInput
+            // 
+            this.TextBoxInput.Location = new System.Drawing.Point(110, 41);
+            this.TextBoxInput.Name = "TextBoxInput";
+            this.TextBoxInput.Size = new System.Drawing.Size(278, 20);
+            this.TextBoxInput.TabIndex = 4;
+            this.TextBoxInput.TextChanged += new System.EventHandler(this.TextBoxInput_TextChanged);
+            // 
+            // ButtonMidExtreme
+            // 
+            this.ButtonMidExtreme.Location = new System.Drawing.Point(42, 358);
+            this.ButtonMidExtreme.Name = "ButtonMidExtreme";
+            this.ButtonMidExtreme.Size = new System.Drawing.Size(114, 37);
+            this.ButtonMidExtreme.TabIndex = 5;
+            this.ButtonMidExtreme.Text = "Mid-Extreme";
+            this.ButtonMidExtreme.UseVisualStyleBackColor = true;
+            // 
+            // ButtonMode
+            // 
+            this.ButtonMode.Location = new System.Drawing.Point(321, 358);
+            this.ButtonMode.Name = "ButtonMode";
+            this.ButtonMode.Size = new System.Drawing.Size(114, 37);
+            this.ButtonMode.TabIndex = 6;
+            this.ButtonMode.Text = "Mode";
+            this.ButtonMode.UseVisualStyleBackColor = true;
+            // 
+            // ButtonAverage
+            // 
+            this.ButtonAverage.Location = new System.Drawing.Point(42, 94);
+            this.ButtonAverage.Name = "ButtonAverage";
+            this.ButtonAverage.Size = new System.Drawing.Size(114, 37);
+            this.ButtonAverage.TabIndex = 7;
+            this.ButtonAverage.Text = "Average";
+            this.ButtonAverage.UseVisualStyleBackColor = true;
+            // 
+            // ButtonRange
+            // 
+            this.ButtonRange.Location = new System.Drawing.Point(321, 94);
+            this.ButtonRange.Name = "ButtonRange";
+            this.ButtonRange.Size = new System.Drawing.Size(114, 37);
+            this.ButtonRange.TabIndex = 8;
+            this.ButtonRange.Text = "Range";
+            this.ButtonRange.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonRange);
+            this.Controls.Add(this.ButtonAverage);
+            this.Controls.Add(this.ButtonMode);
+            this.Controls.Add(this.ButtonMidExtreme);
+            this.Controls.Add(this.TextBoxInput);
+            this.Controls.Add(this.ButtonSort);
+            this.Controls.Add(this.ButtonEdit);
+            this.Controls.Add(this.ButtonSearch);
+            this.Controls.Add(this.ListBoxOutput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox ListBoxOutput;
+        private System.Windows.Forms.Button ButtonSearch;
+        private System.Windows.Forms.Button ButtonEdit;
+        private System.Windows.Forms.Button ButtonSort;
+        private System.Windows.Forms.TextBox TextBoxInput;
+        private System.Windows.Forms.Button ButtonMidExtreme;
+        private System.Windows.Forms.Button ButtonMode;
+        private System.Windows.Forms.Button ButtonAverage;
+        private System.Windows.Forms.Button ButtonRange;
     }
 }
 
--- AstrologicalCalculation/Form1.cs
+++ AstrologicalCalculation/Form1.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace AstrologicalCalculation
 {
@@ -15,6 +17,129 @@ namespace AstrologicalCalculation
         public Form1()
         {
             InitializeComponent();
+            FillArray();
+            DisplayInteractions();
+        }
+
+        static int max = 24;
+        int[] myArray = new int[max];
+
+
+        private void DisplayInteractions()
+        {
+            ListBoxOutput.Items.Clear();
+            foreach (var interactions in myArray)
+            {
+                ListBoxOutput.Items.Add(interactions);
+            }
+
+
+        }
+        private void TextBoxInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListBoxOutput_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (ListBoxOutput.SelectedItem != null)
+            {
+                TextBoxInput.Text = ListBoxOutput.SelectedItem.ToString();
+            }
+        }
+
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            int lowBound = 0;
+            int highBound = max;
+            int target;
+            if (!(Int32.TryParse(TextBoxInput.Text, out target)))
+            {
+                MessageBox.Show("You must enter an integer");
+                return;
+            }
+            while (lowBound <= highBound)
+            {
+                int mid = (lowBound + highBound) / 2;
+                if (myArray[mid] == target)
+                {
+                    MessageBox.Show("Found at index " + mid);
+                    return;
+                }
+                else if (myArray[mid] < target)
+                {
+                    lowBound = mid + 1;
+                }
+                else
+                {
+                    highBound = mid - 1;
+                }
+
+            }
+            MessageBox.Show("Not found. Try again.");
+        }
+
+        private void ButtonEdit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TextBoxInput.Text))
+            {
+                MessageBox.Show("Please type any value on text box");
+                return;
+            }
+
+            if (ListBoxOutput.SelectedItem != null)
+            {
+                int newValue;
+                if (Int32.TryParse(TextBoxInput.Text, out newValue))
+                {
+                    myArray[ListBoxOutput.SelectedIndex] = newValue;
+                    DisplayInteractions();
+                    TextBoxInput.Clear();
+                    TextBoxInput.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid input");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an item from the list.");
+            }
+        }
+
+        private void ButtonSort_Click(object sender, EventArgs e)
+        {
+            for (int outer = 0; outer < max - 1; outer++)
+            {
+                for (int inner = 0; inner < max - 1; inner++)
+                {
+                    if (myArray[inner] > myArray[inner + 1])
+                    {
+                        //Swap routine
+                        int temp = myArray[inner];
+                        myArray[inner] = myArray[inner + 1];
+                        myArray[inner + 1] = temp;
+
+                        DisplayInteractions();
+                        Application.DoEvents();
+                        Thread.Sleep(20);
+                    }
+
+
+                }
+            }
+        }
+        //Method to fill Array with random numbers
+        private void FillArray()
+        {
+            //Create a random number
+            Random rand = new Random();
+            for (int i = 0; i < max; i++)
+            {
+                //Random number0..100
+                myArray[i] = rand.Next(10, 99);
+            }
         }
     }
 }
